0dd9077500f950ee7ba74656961a6fe35930c690
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-08-09 23:04:41 -0300

    chain icon to toggle light-dark theme

--- code.js
+++ code.js
@@ -1,10 +1,17 @@
-const
+const //card related
   cards = document.querySelector(".cards"),
   card = document.querySelector(".card");
-const
+const //navigation bar related
   navBar = document.querySelector(".nav"),
   navParas = document.querySelectorAll(".nav p"),
   navWidthToggle = document.querySelector(".nav > .toggle");
+const // light-dark theme related
+  root = document.querySelector(":root"),
+  chain = document.querySelector(".chain");
+
+
+
+//functions
 
 //duplicate original card x times
 function cloneCard(n){
@@ -14,16 +21,34 @@ function cloneCard(n){
   }
 };
 
+//hide text on nav bar to make it narrow
 function toggleNavBar(){
   navBar.classList.toggle("small-bar");
   navParas.forEach(para => para.classList.toggle("hidden"));
   navWidthToggle.classList.toggle("rotated");
 };
 
+//toggle between light and dark theme
+function toggleTheme(){
+  root.classList.toggle("dark");
+  icons.forEach(icon => icon.classList.toggle("dark"));
+};
+
+
+
 //listeners
 navWidthToggle.addEventListener("click", toggleNavBar);
+
+chain.addEventListener("mousedown", toggleTheme);
+
 
 
 //on load
 cloneCard(15);
 navWidthToggle.classList.toggle("rotated");
+
+
+
+//this needs to be placed after cloneCard function,
+//or cloned icons won't be affected by dark theme
+const icons = document.querySelectorAll(`img[src*="icons"]`);
